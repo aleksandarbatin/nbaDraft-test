240365f7c06aa77956f37bb019a5d2b3bbefcd1b
Aleksandar Batin <aleksandarbatin@gmail.com>
2018-01-18 17:19:16 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/nba-draft.png">
+    <DraftTable/>
+  </div>
+</template>
+
+<script>
+import DraftTable from './components/DraftTable'
+
+export default {
+  name: 'App',
+  components: {
+    DraftTable
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DraftTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <main class="draft-container">
+    <table class="table table-bordered">
+    <thead>
+      <tr>
+        <th>Firstname</th>
+        <th>Lastname</th>
+        <th>Pick</th>
+        <th>Affiliation</th>
+        <th>College/Club</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>1</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>2</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>3</td>
+        <td>john@example.com</td>
+        <td>john@example.com</td>
+      </tr>
+    </tbody>
+  </table>
+  </main>
+</template>
+
+<script>
+import axios from 'axios';
+export default {
+  data () {
+    return {
+      results: ''
+    }
+  },
+  created() {
+    var config = {
+        headers: {'Access-Control-Allow-Origin': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8'}
+    };
+    axios.get('http://stats.nba.com/stats/drafthistory?LeagueID=00',config)
+    .then(response => {
+         this.results = response.data
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }
+} 
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.draft-container {
+  width: 70%;
+  margin: 2rem auto;
+}
+</style>
